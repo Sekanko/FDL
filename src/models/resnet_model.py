e21fb55e2bc67f85b5b29438d79ca28bdd62db91
dmnrcznsk <dmnrcznsk@gmail.com>
2026-01-09 20:48:43 +0100

    naprawiona funkcja do importowania modelu resnet

--- src/models/resnet_model.py
+++ src/models/resnet_model.py
@@ -1,6 +1,6 @@
 import timm
 import torch.nn as nn
 
-def get_cifar_model(num_classes=43):
-    model = timm.create_model("resnet20", pretrained=True, num_classes=num_classes)
+def get_resnet_model(num_classes=43):
+    model = timm.create_model("resnet18", pretrained=True, num_classes=num_classes)
     return model
