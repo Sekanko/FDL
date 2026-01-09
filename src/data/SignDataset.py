9bad8e72c280ac68c4c4a5da847c7d5e9917d5a3
dmnrcznsk <dmnrcznsk@gmail.com>
2026-01-09 22:32:17 +0100

    klasa SignDataset i funkcja dataloader zostaly dostosowane do nowych wymagan i teraz dzialaja w zaleznosci od podanego w argumencie trybu

--- src/data/SignDataset.py
+++ src/data/SignDataset.py
@@ -6,12 +6,18 @@ from torchvision import transforms
 
 
 class SignDataset(Dataset):
-    def __init__(self, df, size):
+    def __init__(self, df, size, mode):
         self.paths = df["Path"].values
         self.labels = df["ClassId"].values
         self.rois = df[["Roi.X1", "Roi.Y1", "Roi.X2", "Roi.Y2"]].values
         self.size = size
+        self.mode = mode
 
+        self.image_transform = transforms.Compose([
+            transforms.Resize(self.size),
+            transforms.ToTensor()
+        ])
+        
     def __len__(self):
         return len(self.paths)
 
@@ -21,20 +27,29 @@ class SignDataset(Dataset):
 
         x1, y1, x2, y2 = self.rois[idx]
 
-        image = image.crop((x1, y1, x2, y2))
+        if self.mode == "detection":
+            w, h = image.size
+            target = torch.tensor([
+                x1 / w,
+                y1 / h, 
+                x2 / w,
+                y2 / h
+            ], dtype=torch.float32)
+            image_tensor = self.image_transform(image)
+            return image_tensor, target
 
-        image_transform = transforms.Compose([
-            transforms.Resize(self.size),
-            transforms.ToTensor()
-        ])
+        elif self.mode == "classification":
+            image = image.crop((x1, y1, x2, y2))
+            image_tensor = self.image_transform(image)
+            label = self.labels[idx]
 
-        image_tensor = image_transform(image)
-        label = self.labels[idx]
-
-        return image_tensor, torch.tensor(label, dtype=torch.long)
+            return image_tensor, torch.tensor(label, dtype=torch.long)
 
 
-def create_dataloaders(data, batch_size=32, size=(224, 244)):
-    dataset = SignDataset(data, size)
+def create_dataloaders(data, batch_size=32, size=(32,32), mode="classification"):
+    modes = ["classification", "detection"]
+    if mode not in modes:
+        raise ValueError(f"Function create_dataloaders() does not contain mode {mode}. Must be: {modes}")
+    dataset = SignDataset(data, size, mode)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return loader
